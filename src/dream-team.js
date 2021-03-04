182e6386948e19d472c5148dd0f3a500a5e61ca6
katiandrews <andryushina.eka@gmail.com>
2021-03-04 20:46:48 +0200

    feat: add carbon dating and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members) {
+  return false;
+ }
+ let arr = [];
+ for (i = 0; i < members.length; i++) {
+   if (typeof members[i] == "string") {
+     members[i] = members[i].trim();
+     members[i] = members[i].toUpperCase();
+     arr.push(members[i][0]);
+   }
+ }
+  return arr.sort().join('');
 };
