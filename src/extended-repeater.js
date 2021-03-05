a4b4288712cb356c4b06d425a104d1294043bfa0
katiandrews <andryushina.eka@gmail.com>
2021-03-05 17:44:02 +0200

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(str);
+  let additionStr;
+  //1st check if addition exist
+  if (options.addition !== undefined) {
+    let arr = [];
+    options.addition = String(options.addition);
+  //add option.addition if it exist
+    additionStr = options.addition;
+
+  //repeat addition as many times as propery says and push it to an array;
+    if (options.additionRepeatTimes) {
+      for (i = 1; i <= options.additionRepeatTimes; i++) {
+        arr.push(options.addition);
+      }
+      //add separator between addition, default or custom
+      if (options.additionSeparator !== undefined) {
+      additionStr = arr.join(`${options.additionSeparator}`);
+    } else {
+      additionStr = arr.join('|');
+    }
+    }
+  //add addition to string;
+    str += additionStr;
+
+  }
+  //check if string should be repeated and repeat with separator
+  if (options.repeatTimes) {
+    let array = [];
+    for (i = 1; i <= options.repeatTimes; i++) {
+      array.push(str);
+    }
+    if (options.separator !== undefined) {
+      str = array.join(`${options.separator}`);
+    } else {
+      str = array.join('+');
+    }
+  }
+  return str;
 };
   
