f7d090250151d5880ba806194b1bc36c6d13927e
katiandrews <andryushina.eka@gmail.com>
2021-03-10 20:26:43 +0200

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,26 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor() {
-
+  constructor(type) {
+    this.type = type;
   }
   encrypt(message, key) {
     if (!message || !key) {
       throw new Error ('Error!');
     }
-    //
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let currentKeyIndex = 0;
+    let encrypted = '';
+    
     for (i = 0; i < message.length; i++) {
-      if (charCodeAt(message[i]) >= 65 && charCodeAt(message[i]) <= 90) {
-        i % key.length;
-      } else if (charCodeAt(message[i]) >= 97 && charCodeAt(message[i]) <= 122 ) {
-
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) { //letter A-Z
+        let letterCode = message.charCodeAt(i); 
+        let keyLetterCode = key.charCodeAt(currentKeyIndex); 
+        encrypted += String.fromCharCode((letterCode + keyLetterCode) % 26 + 65);
+        currentKeyIndex = (currentKeyIndex + 1) % key.length; //if message[i] is a letter, than we shift keyIndex to next key symbol;
+      } else {
+        encrypted += message.charAt(i);
       }
     }
+    if (this.type === false) {
+      return encrypted.split('').reverse().join('');
+    }
+    return encrypted;
   }    
+  
   decrypt(encryptedMessage, key) {
     if (!encryptedMessage || !key) {
       throw new Error ('Error!');
     }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let currentKeyIndex = 0;
+    let decrypted = '';
+    
+    for (i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage.charCodeAt(i) >= 65 && encryptedMessage.charCodeAt(i) <= 90) { //letter A-Z
+        let letterCode = encryptedMessage.charCodeAt(i) - 65; //number 0-25
+        let keyLetterCode = key.charCodeAt(currentKeyIndex) - 65; //number 0-25
+        if (letterCode - keyLetterCode >= 0) {
+          decrypted += String.fromCharCode(Math.abs(letterCode - keyLetterCode) % 26 + 65);
+        } else {
+          decrypted += String.fromCharCode(91 + (letterCode - keyLetterCode)% 26);
+        }
+        currentKeyIndex = (currentKeyIndex + 1) % key.length; //if message[i] is a letter, than we shift keyIndex to next key symbol;
+      } else {
+        decrypted += encryptedMessage.charAt(i);
+      }
+    }
+    if (this.type === false) {
+      return decrypted.split('').reverse().join('');
+    }
+    return decrypted;
   }
 }
 
